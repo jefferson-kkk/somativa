fbb89aa2fb9a3257cb6a076fc001e1d8b3070d77
jefferson-kkk <jecorrea.2008@gmail.com>
2026-05-24 22:41:56 -0300

    somativa

--- bootstrap/providers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\Filament\PortariaPanelProvider::class,
-    App\Providers\Filament\ProfessorPanelProvider::class,
-];
